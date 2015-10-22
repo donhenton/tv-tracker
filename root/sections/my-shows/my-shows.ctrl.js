8ee8f27fe1e407ce30c4cb9450efeaed8545d06e
d <dhenton@localhost.com>
2015-10-22 08:27:53 -0500

    stuff

--- root/sections/my-shows/my-shows.ctrl.js
+++ root/sections/my-shows/my-shows.ctrl.js
@@ -1,4 +1,4 @@
 angular.module('app.core').controller('MyShowsController', function(StoreFactory){
     var vm = this;
-    vm.results = StoreFactory.getShows();
+     vm.results = StoreFactory.getShows();
 });
